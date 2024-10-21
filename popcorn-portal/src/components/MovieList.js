43cd0e09e1e2e6dc47a14f49df9e73696524c5e9
aram <6aram@naver.com>
2024-10-21 15:40:19 +0900

    Material-ui를 통해 movie List 화면 수정

--- popcorn-portal/src/components/MovieList.js
+++ popcorn-portal/src/components/MovieList.js
@@ -2,9 +2,11 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux"; // Redux 사용
 import { fetchMovies } from "../slices/movieSlice";
 import Movies from "./Movies";
-import { Box } from "@mui/material"; // Material-UI Box 사용
+import { experimentalStyled as styled } from "@mui/material/styles"; // Material styles 사용
+import { Box, Paper, Grid2 } from "@mui/material"; // Material-UI Box, Paper, Grid2 사용
 
 const MovieList = () => {
+  // 데이터 전달
   const dispatch = useDispatch();
   const { items, loading, error } = useSelector((state) => state.movies);
 
@@ -12,23 +14,46 @@ const MovieList = () => {
     dispatch(fetchMovies());
   }, [dispatch]);
 
+  // 리스트 Style 설정
+  const Boxes = styled(Paper)(({ theme }) => ({
+    backgroundColor: "#FFF",
+    ...theme.typography.body2,
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    ...theme.applyStyles("dark", {
+      backgroundColor: "#1A2027",
+    }),
+    height: "500px", // 고정 높이 설정
+    display: "flex", // flexbox 레이아웃 설정
+    flexDirection: "column", // 세로로 정렬
+    justifyContent: "flex-start", // 시작점에서 정렬
+  }));
+
   if (loading) return <p>로딩중...</p>;
   if (error) return <p>Error: {error}</p>;
 
   return (
-    <div>
-      <h1>팝콘 Movie List</h1>
-      {items.map((movie) => (
-        <Movies
-          key={movie.id} // React 내 고유 key
-          id={movie.id} // 컴포넌트에서 사용할 Id값
-          coverImg={movie.medium_cover_image}
-          title={movie.title}
-          summary={movie.summary}
-          genres={movie.genres}
-        />
-      ))}
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <h1>PopCorn Portal Movie List</h1>
+      <Grid2 container spacing={2} columns={16}>
+        {items.map((movie) => (
+          <Grid2 xs={12} sm={6} md={4} lg={3} key={movie.id}>
+            <Boxes sx={{ width: "100%", maxWidth: "250px" }}>
+              {/** 각 영화 정보를 보여주는 Movies 컴포넌트 */}
+              <Movies
+                key={movie.id} // React 내 고유 key
+                id={movie.id} // 컴포넌트에서 사용할 Id값
+                coverImg={movie.medium_cover_image}
+                title={movie.title}
+                summary={movie.summary}
+                genres={movie.genres}
+              />
+            </Boxes>
+          </Grid2>
+        ))}
+      </Grid2>
+    </Box>
   );
 };
 
--- popcorn-portal/src/components/Movies.js
+++ popcorn-portal/src/components/Movies.js
@@ -1,21 +1,58 @@
 import PropTypes from "prop-types";
+import { Box, Typography } from "@mui/material";
 
 //@TODO : 상세 설명을 한글로 번역, 요약해주는 AI 추가
 
 //Movies : 영화 상세 정보 컴포넌트
 function Movies({ id, coverImg, title, summary, genres }) {
   return (
-    <div id={id}>
-      <img src={coverImg} alte={title} />
-      <h2>{title}</h2>
-      <p>{summary}</p>
-      <ul>
-        {genres.map((g) => (
-          <li key={`${id}-${g}`}>{g}</li>
-        ))}
-      </ul>
-      <hr />
-    </div>
+    <Box sx={{ flexGrow: 1, overflow: "hidden" }}>
+      <img
+        src={coverImg}
+        alt={title}
+        style={{
+          width: "100%",
+          height: "auto",
+          maxHeight: "400px",
+          objectFit: "cover",
+          marginBottom: "5px",
+        }}
+      />
+      <Typography
+        variant="h6"
+        noWrap
+        sx={{
+          overflow: "hidden",
+          textOverflow: "ellipsis",
+          marginBottom: "5px",
+        }}
+      >
+        {title}
+      </Typography>
+      <Typography
+        variant="body2"
+        sx={{
+          overflow: "hidden",
+          textOverflow: "ellipsis",
+          display: "-webkit-box",
+          WebkitLineClamp: 3, // summary의 최대 줄 수 설정
+          WebkitBoxOrient: "vertical",
+        }}
+      >
+        {summary}
+      </Typography>
+    </Box>
+
+    // <div id={id}>
+    //   <img src={coverImg} alte={title} />
+    //   <h2>{title}</h2>
+    //   <p>{summary}</p>
+    //   <ul>
+    //     {genres.map((g) => (
+    //       <li key={`${id}-${g}`}>{g}</li>
+    //     ))}
+    //   </ul>
+    // </div>
   );
 }
 
