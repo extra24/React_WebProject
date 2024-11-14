cba1aa9c3e26eea430ecf12028446d9997fe0880
aram <6aram@naver.com>
2024-11-14 15:02:43 +0900

    python backend - CORS 설정 추가

--- server/app.py
+++ server/app.py
@@ -1,9 +1,14 @@
 from flask import Flask
-from routes.movie_routes import movie_blueprint
+from flask_cors import CORS # CORS 추가
+from routes.movieRoutes import movie_blueprint
 
 '''Flask 애플리케이션 실행하고, routes 폴더의 앤드포인트를 등록'''
 
 app = Flask(__name__)
+
+# 모든 출처에서 CORS를 허용
+CORS(app)
+
 app.register_blueprint(movie_blueprint)
 
 # 메인 함수 - 서버 실행 및 디버그 모드 활성화
